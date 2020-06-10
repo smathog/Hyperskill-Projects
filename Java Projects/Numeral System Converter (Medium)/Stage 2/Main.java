c7e7c6f4a4440b05897cc14441ae9690d3fa9d34
Scott Mathog <smathog@gmail.com>
2020-06-10 00:04:51 -0700

    Finished Stage 2 of Numeral System Converter

---
+++ Java Projects/Numeral System Converter (Medium)/Stage 2/Main.java
@@ -0,0 +1,11 @@
+package converter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(num % 8); 
+    }
+}
---
+++ Java Projects/Numeral System Converter (Medium)/Stage 2/README.md
@@ -0,0 +1,5 @@
+The stage specification can be found [here](https://hyperskill.org/projects/41/stages/215/implement)
+
+## Thoughts
+
+Another easy stage; just output the number mod 8. 
