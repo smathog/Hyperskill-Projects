99aad383fbeb07acec222bb67fa9545a2bb102b4
Scott Mathog <smathog@gmail.com>
2020-12-24 16:46:24 -0800

    Finished Stage 3 of Code Sharing Platform

---
+++ Java Projects/Code Sharing Platform (Challenging)/Stage 3/returnID.java
@@ -0,0 +1,17 @@
+package platform;
+
+public class returnID {
+    private String id;
+
+    public returnID(String id) {
+        setId(id);
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
