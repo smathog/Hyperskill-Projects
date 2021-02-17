1c6417a2617aacebc4aba7e9f923addf55c51083
Scott <smathog@gmail.com>
2021-02-16 22:27:12 -0800

    Finished Stage 1 of Loan Calculator

---
+++ Python Projects/Loan Calculator (Easy)/Stage 1/creditcalc.py
@@ -0,0 +1,10 @@
+loan_principal = 'Loan principal: 1000'
+final_output = 'The loan has been repaid!'
+first_month = 'Month 1: repaid 250'
+second_month = 'Month 2: repaid 250'
+third_month = 'Month 3: repaid 500'
+
+# write your code here
+list_ = [loan_principal, first_month, second_month, third_month, final_output]
+for i in list_:
+    print(i)
