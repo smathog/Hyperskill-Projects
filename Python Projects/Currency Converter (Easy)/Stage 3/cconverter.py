c9900511fa92d6814a6d1bff13f10535905f94bc
Scott <smathog@gmail.com>
2021-02-23 15:56:33 -0800

    Finished Stage 3 of Currency Converter

---
+++ Python Projects/Currency Converter (Easy)/Stage 3/cconverter.py
@@ -0,0 +1,3 @@
+n = int(input("Please, enter the number of conicoins you have: "))
+exchange_rate = float(input("Please, enter the exchange rate: "))
+print(f"The total amount of dollars: {n * exchange_rate}")
