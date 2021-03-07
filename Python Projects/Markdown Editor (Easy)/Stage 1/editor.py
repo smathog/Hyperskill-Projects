719c75e0e158541e7cae5a35aebd9aa835316835
Scott Mathog <smathog@gmail.com>
2021-03-07 11:21:14 -0800

    Finished Stage 1 of Markdown Editor

---
+++ Python Projects/Markdown Editor (Easy)/Stage 1/editor.py
@@ -0,0 +1,10 @@
+markdown_str = """# John Lennon
+or ***John Winston Ono Lennon*** was one of *The Beatles*.
+Here are the songs he wrote I like the most:
+* Imagine
+* Norwegian Wood
+* Come Together
+* In My Life
+* ~~Hey Jude~~ (that was *McCartney*)"""
+
+print(markdown_str)
